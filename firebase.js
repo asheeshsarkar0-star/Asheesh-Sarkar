412bb4fd01d5966b63fb0e5321f337cef3016f68
Asheesh Sarkar <asheeshsarkar0@gmail.com>
2026-01-16 23:47:50 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,12 @@
-{
-  "rules": {
-    ".read": true,
-    ".write": true
-  }
-}
+const firebaseConfig={
+  apiKey:"YOUR_KEY",
+  authDomain:"PROJECT.firebaseapp.com",
+  projectId:"PROJECT",
+  storageBucket:"PROJECT.appspot.com",
+  databaseURL:"https://PROJECT.firebaseio.com"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db=firebase.database();
+const storage=firebase.storage();

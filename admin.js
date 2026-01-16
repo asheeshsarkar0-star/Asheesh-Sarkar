cbb7700073fab4814d6ab095c52f4c9de324b652
Asheesh Sarkar <asheeshsarkar0@gmail.com>
2026-01-16 23:49:38 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,27 +1,12 @@
-const PASSWORD = "asheesh@admin"; // 🔥 CHANGE THIS
+function upload(){
+  const file=document.getElementById("file").files[0];
+  const type=document.getElementById("type").value;
 
-function login() {
-  const pass = document.getElementById("adminPass").value;
-  if (pass === PASSWORD) {
-    document.getElementById("panel").style.display = "block";
-    alert("Login Successful");
-  } else {
-    alert("Wrong Password");
-  }
-}
-
-function uploadMedia() {
-  const url = document.getElementById("mediaUrl").value;
-  const type = document.getElementById("mediaType").value;
-
-  if (!url) return alert("Enter URL");
-
-  db.ref("gallery").push({
-    url,
-    type,
-    time: Date.now()
+  const ref=storage.ref("media/"+Date.now()+file.name);
+  ref.put(file).then(()=>{
+    ref.getDownloadURL().then(url=>{
+      db.ref("media").push({url,type});
+      alert("Uploaded");
+    });
   });
-
-  alert("Uploaded Successfully");
-  document.getElementById("mediaUrl").value = "";
 }

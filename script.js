97e61469e362d9e239c201c95feaf96944025f98
Asheesh Sarkar <asheeshsarkar0@gmail.com>
2026-01-16 23:47:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
-// Smooth Scroll
-document.querySelectorAll('a[href^="#"]').forEach(a=>{
-a.onclick=e=>{
-e.preventDefault();
-document.querySelector(a.getAttribute("href"))
-.scrollIntoView({behavior:"smooth"});
+function openMenu(){
+  document.getElementById("mobileMenu").style.transform="translateX(0)";
 }
-});
+function closeMenu(){
+  document.getElementById("mobileMenu").style.transform="translateX(100%)";
+}
 
-// LOAD GALLERY
-const gallery = document.getElementById("gallery");
+const gallery=document.getElementById("gallery");
 
-if (gallery) {
-  db.ref("gallery").on("value", snap => {
-    gallery.innerHTML = "";
-    snap.forEach(item => {
-      const data = item.val();
-      if (data.type === "image") {
-        gallery.innerHTML += `<img src="${data.url}" style="width:100%;border-radius:15px">`;
-      } else {
-        gallery.innerHTML += `<iframe src="${data.url}" style="width:100%;height:200px;border-radius:15px"></iframe>`;
-      }
-    });
+db.ref("media").on("value",snap=>{
+  gallery.innerHTML="";
+  snap.forEach(item=>{
+    const d=item.val();
+    if(d.type==="image")
+      gallery.innerHTML+=`<img src="${d.url}" style="width:100%">`;
+    if(d.type==="audio")
+      gallery.innerHTML+=`<audio controls src="${d.url}"></audio>`;
+    if(d.type==="video")
+      gallery.innerHTML+=`<video controls src="${d.url}"></video>`;
   });
-}
+});

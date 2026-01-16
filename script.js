c4a079c5168588790692da19e3e6d83665617d9c
Asheesh Sarkar <asheeshsarkar0@gmail.com>
2026-01-16 23:08:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Smooth scroll
+// Smooth Scroll
 document.querySelectorAll('a[href^="#"]').forEach(a=>{
 a.onclick=e=>{
 e.preventDefault();
@@ -6,3 +6,20 @@ document.querySelector(a.getAttribute("href"))
 .scrollIntoView({behavior:"smooth"});
 }
 });
+
+// LOAD GALLERY
+const gallery = document.getElementById("gallery");
+
+if (gallery) {
+  db.ref("gallery").on("value", snap => {
+    gallery.innerHTML = "";
+    snap.forEach(item => {
+      const data = item.val();
+      if (data.type === "image") {
+        gallery.innerHTML += `<img src="${data.url}" style="width:100%;border-radius:15px">`;
+      } else {
+        gallery.innerHTML += `<iframe src="${data.url}" style="width:100%;height:200px;border-radius:15px"></iframe>`;
+      }
+    });
+  });
+}

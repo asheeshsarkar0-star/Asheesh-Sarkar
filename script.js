65329681b1230869d1aa585529cdb40355fd8bd4
Asheesh Sarkar <asheeshsarkar0@gmail.com>
2026-01-16 22:54:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// Smooth scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.onclick = function(e) {
     e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
+    document.querySelector(this.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  };
 });

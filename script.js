2afa31753e9703926a0e3c18b987532078e4386a
Asheesh Sarkar <asheeshsarkar0@gmail.com>
2026-01-17 00:03:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,11 @@ function closeMenu(){
   document.getElementById("mobileMenu").style.transform="translateX(100%)";
 }
 
-const gallery=document.getElementById("gallery");
+function openItem(name){
+  alert(name + " open hoga (future me full page / upload system)");
+}
 
-db.ref("media").on("value",snap=>{
-  gallery.innerHTML="";
-  snap.forEach(item=>{
-    const d=item.val();
-    if(d.type==="image")
-      gallery.innerHTML+=`<img src="${d.url}" style="width:100%">`;
-    if(d.type==="audio")
-      gallery.innerHTML+=`<audio controls src="${d.url}"></audio>`;
-    if(d.type==="video")
-      gallery.innerHTML+=`<video controls src="${d.url}"></video>`;
-  });
-});
+function sendMessage(e){
+  e.preventDefault();
+  alert("Message sent successfully!");
+}

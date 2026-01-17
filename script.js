95bcea5ea90ba9d155b9b704d9181eed4252c244
Asheesh Sarkar <asheeshsarkar0@gmail.com>
2026-01-17 20:17:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function sendMessage(e){
-  e.preventDefault();
-  alert("Message sent successfully!");
+function setVH(){
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
+setVH();
+window.addEventListener('resize', setVH);

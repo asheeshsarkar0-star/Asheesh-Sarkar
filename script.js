cc79497b19d9c0701aedc96cae60915a88d8bb39
Asheesh Sarkar <asheeshsarkar0@gmail.com>
2026-01-17 12:09:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,19 @@
 function openMenu(){
-  document.getElementById("mobileMenu").style.transform="translateX(0)";
+  document.getElementById("mobileMenu").style.right="0";
 }
 function closeMenu(){
-  document.getElementById("mobileMenu").style.transform="translateX(100%)";
+  document.getElementById("mobileMenu").style.right="-100%";
+}
+
+function openImg(src){
+  document.getElementById("viewer").style.display="flex";
+  document.getElementById("viewerImg").src=src;
+}
+function closeImg(){
+  document.getElementById("viewer").style.display="none";
 }
 
 function sendMessage(e){
   e.preventDefault();
   alert("Message sent successfully!");
 }
-
-/* FIREBASE MEDIA LOAD */
-const gallery = document.getElementById("gallery");
-
-firebase.database().ref("media").on("value", snap=>{
-  gallery.innerHTML="";
-  snap.forEach(item=>{
-    const d=item.val();
-
-    if(d.type==="image"){
-      gallery.innerHTML += `<img src="${d.url}" style="width:100%;border-radius:12px">`;
-    }
-
-    if(d.type==="video"){
-      gallery.innerHTML += `<video controls style="width:100%"><source src="${d.url}"></video>`;
-    }
-
-    if(d.type==="audio"){
-      gallery.innerHTML += `<audio controls style="width:100%"><source src="${d.url}"></audio>`;
-    }
-  });
-});

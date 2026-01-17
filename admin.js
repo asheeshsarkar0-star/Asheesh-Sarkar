92ec3a73af3e2cdca34273f9adc00241d44eca63
Asheesh Sarkar <asheeshsarkar0@gmail.com>
2026-01-17 11:43:25 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,12 +1,13 @@
 function upload(){
-  const file=document.getElementById("file").files[0];
-  const type=document.getElementById("type").value;
+  const file = document.getElementById("file").files[0];
+  const type = document.getElementById("type").value;
 
-  const ref=storage.ref("media/"+Date.now()+file.name);
+  const ref = storage.ref("media/" + Date.now() + file.name);
+
   ref.put(file).then(()=>{
     ref.getDownloadURL().then(url=>{
       db.ref("media").push({url,type});
-      alert("Uploaded");
+      alert("Uploaded Successfully");
     });
   });
 }

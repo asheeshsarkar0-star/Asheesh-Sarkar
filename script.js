993a0104775a39cce203b625a30a325e3884c1c0
Asheesh Sarkar <asheeshsarkar0@gmail.com>
2026-01-17 11:41:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,29 @@ function closeMenu(){
   document.getElementById("mobileMenu").style.transform="translateX(100%)";
 }
 
-function openItem(name){
-  alert(name + " open hoga (future me full page / upload system)");
-}
-
 function sendMessage(e){
   e.preventDefault();
   alert("Message sent successfully!");
 }
+
+/* FIREBASE MEDIA LOAD */
+const gallery = document.getElementById("gallery");
+
+firebase.database().ref("media").on("value", snap=>{
+  gallery.innerHTML="";
+  snap.forEach(item=>{
+    const d=item.val();
+
+    if(d.type==="image"){
+      gallery.innerHTML += `<img src="${d.url}" style="width:100%;border-radius:12px">`;
+    }
+
+    if(d.type==="video"){
+      gallery.innerHTML += `<video controls style="width:100%"><source src="${d.url}"></video>`;
+    }
+
+    if(d.type==="audio"){
+      gallery.innerHTML += `<audio controls style="width:100%"><source src="${d.url}"></audio>`;
+    }
+  });
+});

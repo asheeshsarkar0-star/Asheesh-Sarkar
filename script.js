92559a3bd018fa060d6786e5cc045539142a261e
Asheesh Sarkar <asheeshsarkar0@gmail.com>
2026-01-17 20:22:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,17 @@ function setVH(){
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
+
+setVH();
+window.addEventListener('resize', setVH);
+
+function sendMessage(e){
+  e.preventDefault();
+  alert("Message sent successfully!");
+}
+function setVH(){
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
 setVH();
 window.addEventListener('resize', setVH);
